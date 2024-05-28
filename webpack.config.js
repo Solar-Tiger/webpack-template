09e0e921edb30a10d3ae6e073233fd2c1fa5a588
David Singleton <davidjsingleton18@gmail.com>
2024-05-28 11:01:17 -0700

    Change location of certain fields and add more dev specific tools for development

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Todo Proejct',
-      template: './src/index.html',
-    }),
-  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
@@ -23,6 +23,18 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
