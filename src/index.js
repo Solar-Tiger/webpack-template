28bdec685195db9e3efccef9309319b14e185aa3
David Singleton <davidjsingleton18@gmail.com>
2024-05-31 11:02:19 -0700

    Add svg to document body

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
 
 console.log('Hello');
+
+const div = document.createElement('div');
+
+const img = new Image();
+
+img.src = './assets/images/icons/edit_note.svg';
+
+div.appendChild(img);
+
+document.body.appendChild(div);
