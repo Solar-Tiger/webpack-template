8ddd3e905f11461e7772be11a7d9ac60a002d91f
David Singleton <davidjsingleton18@gmail.com>
2024-06-06 11:00:43 -0700

    Remove components again as it is not needed
    Add a modules folder to separate different javascript functions and logic

--- src/components/DeleteAccountModal.js
+++
@@ -1,9 +0,0 @@
-// This is a component made up of small components
-
-// Since it's a "Delete Account Modal", it probably contains things related to a button, a text input and a modal. We'd have to import all 3 of these to make this component.
-
-import btn from './ui/Button.js';
-import form from './ui/Modal.js';
-import textInput from './ui/TextInput.js';
-
-// And now we can begin putting together a "Delete Account Modal"
