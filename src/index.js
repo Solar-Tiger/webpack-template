684535803bdebe2357f2da526e6113cddcee43d5
David Singleton <davidjsingleton18@gmail.com>
2024-06-12 10:10:49 -0700

    Move background color style to style file to keep things separate and manageable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
 import editSVG from './assets/images/icons/edit_note.svg';
 
-document.body.style.backgroundColor = '#ddd';
-
 console.log('Hello');
 
 const div = document.createElement('div');
