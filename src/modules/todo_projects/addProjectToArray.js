1a419ba8b03f8611f0c1c4eb0172ea33a3f10e16
David Singleton <davidjsingleton18@gmail.com>
2024-06-12 10:58:46 -0700

    Update the way I create and add project todos to the array with unique ids

--- src/modules/todo_projects/addProjectToArray.js
+++ src/modules/todo_projects/addProjectToArray.js
@@ -1,17 +1,13 @@
 import { createTodoProject } from './createTodoProject';
 
-// Array to store project
-const todoProjects = [];
-
 // Add TODO projects to Array
-function addTodoToArray(userInput) {
-  const newTodoProject = createTodoProject(userInput);
+export function addTodoToArray(projectTitle, todoProjects) {
+  const newTodoProject = createTodoProject({
+    projectTitle,
+  });
 
   // For debugging purposes
   console.log(newTodoProject);
 
   todoProjects.push(newTodoProject);
 }
-
-// EXAMPLE
-addTodoToArray('Food to make');
--- src/modules/todo_projects/createTodoProject.js
+++ src/modules/todo_projects/createTodoProject.js
@@ -1,40 +1,10 @@
+import { createId } from '@paralleldrive/cuid2';
+
 // Create TODO projects
 export function createTodoProject(title) {
   return {
-    id: Date.now(),
+    id: createId(), // Generate unique ID
     projectTitle: title,
     task: [],
   };
 }
-
-// Create TODO task
-function createTodoTask(title, description, dueDate, priority) {
-  return {
-    title,
-    description,
-    dueDate,
-    priority,
-  };
-}
-
-// Add TODO task to Project Task Array
-function addTodoTaskToArray(
-  userTitle,
-  userDescription,
-  userDueDate,
-  userPriority
-) {
-  const newTask = createTodoTask(
-    userTitle,
-    userDescription,
-    userDueDate,
-    userPriority
-  );
-
-  // For debugging purposes
-  console.log(newTask);
-
-  todoProjects[0].task.push(newTask);
-}
-
-addTodoTaskToArray('Tuna salad', 'A simple tuna salad', new Date(), 'HIGH');
