f524ef4da502cd34ec66f087c155bbca7fbf6ff1
David Singleton <davidjsingleton18@gmail.com>
2025-08-05 10:55:10 -0700

    Update webpack common to use import statements from require

--- webpack.common.js
+++ webpack.common.js
@@ -1,38 +1,37 @@
-const path = require('path');
-// eslint-disable-next-line import/no-extraneous-dependencies, node/no-unpublished-require
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import pate from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset',
-      },
+export default {
+    entry: {
+        app: './src/index.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
     ],
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset'
+            }
+        ]
+    },
+    optimization: {
+        runtimeChunk: 'single'
+    }
 };
