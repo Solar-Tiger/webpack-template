86c421a94d43866cd672459dca797a9246a69842
David Singleton <davidjsingleton18@gmail.com>
2025-08-06 10:11:34 -0700

    Remove unneeded require statements

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,6 @@
 import { merge } from 'webpack-merge';
 import common from './webpack.common.js';
 
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-
 export default merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
